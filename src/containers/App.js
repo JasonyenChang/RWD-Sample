e0789c720a813c8a6c044d752832a53b4308a89a
Jason <jasonyen_chang@wistron.com>
2019-04-29 13:17:24 +0800

    Adding babelrc and webpack config

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,8 +3,46 @@ import React from 'react';
 export default class App extends React.Component {
   render() {
     return (
-      <div style={{ textAlign: 'center', color: 'blue' }}>
-        <h2>Hello, I`m React start kit!!!!</h2>
+      <div className="pure-g">
+        <div className="pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+          <div className="l-box">
+            <h3>Lorem Ipsums</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua. Ut enim ad minim veniam.
+        </p>
+          </div>
+        </div>
+
+        <div className="pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+          <div className="l-box">
+            <h3>Dolor Sit Amet</h3>
+            <p>
+              Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse.
+        </p>
+          </div>
+        </div>
+
+        <div className="pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+          <div className="l-box">
+            <h3>Proident laborum</h3>
+            <p>
+              In culpa qui officia deserunt mollit anim id est laborum. incididunt ut labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamco.
+        </p>
+          </div>
+        </div>
+
+        <div className="pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+          <div className="l-box">
+            <h3>Praesent consectetur</h3>
+            <p>
+              Integer vitae lectus accumsan, egestas dui eget, ullamcorper urn. In feugiat tortor at turpis rhoncus
+              tincidunt. Duis sed porttitor ante, eget venenatis lectus.
+        </p>
+          </div>
+        </div>
       </div>
     );
   }
